7cd4bdf0f82784693ad5e8119d0dab9a9a4ea9ef
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-02-21 12:36:19 +0530

    added url for gender_detection_view

--- src/ScanSecure/urls.py
+++ src/ScanSecure/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import detect_people_view
+from django.conf.urls.static import static
+from .views import *
+from django.conf import settings
 
 urlpatterns = [
     path("detect/", detect_people_view, name="detect_people"),
+    path('detect-gender/', gender_detection_view, name='gender-detection'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
