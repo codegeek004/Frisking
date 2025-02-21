193043cdc8324ca3c91fb32870af5933fffc5270
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-02-21 12:09:49 +0530

    added people detection view for template

--- src/ScanSecure/views.py
+++ src/ScanSecure/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+import os
+from .detect_people import detect_people
 
-# Create your views here.
+def detect_people_view(request):
+    context = {"detected_people": None, "output_image": None}
+    
+    if request.method == "POST" and request.FILES.get("image"):
+        image = request.FILES["image"]
+        fs = FileSystemStorage(location="ScanSecure/static/uploads")
+        image_path = fs.save(image.name, image)
+        full_image_path = os.path.join("ScanSecure/static/uploads", image_path)
+
+        detected_people, output_image_path = detect_people(full_image_path)
+        context["detected_people"] = detected_people
+        context["output_image"] = output_image_path.replace("ScanSecure/static/", "")
+
+    return render(request, "upload_photo.html", context)
