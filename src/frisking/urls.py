babe651b3ee10431a4cfc83a98539ac81dd02dba
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2025-02-24 11:06:41 +0530

    added allauth urls

--- src/frisking/urls.py
+++ src/frisking/urls.py
@@ -1,25 +1,23 @@
-"""
-URL configuration for frisking project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
+#here allauth.urls fails to respond when it is after the scansecure.urls because there is regex in a url in scansecure app
 urlpatterns = [
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
-    path('', include('ScanSecure.urls'))
+    path('', include('ScanSecure.urls')),
+      # Ensure this is included
 ]
 
+# # If you still need to serve static files in DEBUG mode
+# if settings.DEBUG:
+#     from django.views.static import serve
+#     from django.urls import re_path
 
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#     # REMOVE or MODIFY this to avoid conflicts
+#     urlpatterns += [
+#         re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+#     ]
