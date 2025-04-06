b8f9044db3282cb1dee4432e911c66e71f6167fd
codegeek004 <yashvaishnav1404@gmail.com>
2025-04-07 00:35:01 +0530

    added enquiry email and detect_both script url

--- src/ScanSecure/urls.py
+++ src/ScanSecure/urls.py
@@ -6,7 +6,9 @@ from django.conf import settings
 urlpatterns = [
     path('detect-gender/', gender_detection_view, name='gender_detection'),
     path("sign-detection/", sign_detection_view, name="sign_detection"),
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('demo/', detect_both, name="demo"),
+    path('query/', send_enquiry_mail, name="enquiry")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
